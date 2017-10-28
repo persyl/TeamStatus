3f1c3d1c0c9a4d4d7e7d7b5e9291f76a4c8553d3
Per Lundkvist <per.lundkvist@di.se>
2017-10-28 09:02:23 +0200

    Extracting CSS from LESS

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Company from './components/company';
 
+import './app.less';
+
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className='ts_main'>
             <h1>Team Status App</h1>
             <Company />
             </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,13 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+const extractLess = new ExtractTextPlugin({
+    filename: "[name].css", //"[name].[contenthash].css"
+    //disable: process.env.NODE_ENV === "development"
+});
+
+const ManifestPlugin = require('webpack-manifest-plugin');
+
 module.exports = {
     context: __dirname + "/",
     entry: "./index.js",
@@ -20,7 +30,25 @@ module.exports = {
                         presets: ['react', 'env']
                     }
                 }
+            },
+
+            {
+                test: /\.less$/,
+                use: extractLess.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "less-loader"
+                    }],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
             }
         ]
-    }
+    },
+
+    plugins: [
+        new ManifestPlugin({ fileName: 'build-manifest.json' }),
+        extractLess
+    ]
 };
