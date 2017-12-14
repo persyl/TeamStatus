aec36a498ae2b0e38fd5c5fe0472ba84d9d6e269
Per Lundkvist <per.lundkvist@di.se>
2017-12-14 09:43:38 +0100

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     },
     output: {
         path: path.join(__dirname, "/dist"),
-        publicPath: "/dist",
+        publicPath: "/",
         filename: "bundle.js"
     },
     resolve: {
